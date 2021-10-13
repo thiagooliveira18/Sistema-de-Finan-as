1292565b62a6ba64eec77dded6555f5a8d9e3fd3
Thiago Oliveira <62665295+thiagooliveira18@users.noreply.github.com>
2021-10-13 14:29:16 -0300

    data display completed

--- src/helpers/dateFilter.ts
+++ src/helpers/dateFilter.ts
@@ -1,4 +1,5 @@
 import { Item } from "../types/Item";
+
 export const getCurrentMonth = () => {
     let now = new Date();
     return `${now.getFullYear()}-${now.getMonth() + 1}`;
@@ -20,3 +21,13 @@ export const filterListByMonth = (list: Item[], date: string): Item[] => {
 
     return newList;
 }
+
+export const formatDate = (date: Date): string => {
+    let year = date.getFullYear();
+    let month = date.getMonth();
+    let day = date.getDate();
+
+    return `${addZeroToDate(day)}/${addZeroToDate(month)}/${year}`;
+}
+
+const addZeroToDate = (n: number): string => n < 10 ? `0${n}` : `${n}`;
